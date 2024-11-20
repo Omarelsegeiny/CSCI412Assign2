e57e540fa4e1d1d2957aa5f94cb4f88d96f2761d
Omarelsegeiny <131826891+Omarelsegeiny@users.noreply.github.com>
2024-11-19 22:30:39 -0600

    finish assignment 6 (bug fixes)

--- app/src/main/java/com/example/csci412assign2/MainActivity.kt
+++ app/src/main/java/com/example/csci412assign2/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.csci412assign2
 
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.csci412assign2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     companion object {
         private const val PERMISSION_REQUEST_CODE = 1001
@@ -24,26 +34,43 @@ class MainActivity : AppCompatActivity() {
         val implicitButton = findViewById<Button>(R.id.button_implicit)
         val viewImageButton = findViewById<Button>(R.id.button_view_image)
 
-        // Explicit Intent (Triggered on button click)
-        explicitButton.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, "com.example.csci412assign2.MSE412") != PackageManager.PERMISSION_GRANTED) {
-                // Request permission if not granted
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf("com.example.csci412assign2.MSE412"),
-                    PERMISSION_REQUEST_CODE
-                )
-
+        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                startSecondActivity()
             } else {
-                val explicitIntent = Intent(this, SecondActivity::class.java)
-                startActivity(explicitIntent)
+                displayToast("Permission denied. Cannot access Second Activity.")
             }
+        }
+
+        if (ContextCompat.checkSelfPermission(this, "com.example.csci412assign2.MSE412") == PackageManager.PERMISSION_GRANTED) {
+            startSecondActivity()
+        } else {
+            requestPermissionLauncher.launch("com.example.csci412assign2.MSE412")
+        }
+
+        explicitButton.setOnClickListener {
+            requestPermissionLauncher.launch("com.example.csci412assign2.MSE412")
         }
 
         // Implicit Intent
         implicitButton.setOnClickListener {
-            val implicitIntent = Intent(Intent.ACTION_VIEW)
+            val implicitIntent =  Intent()
             startActivity(implicitIntent)
+            
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            setSupportActionBar(binding.toolbar)
+
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
+            appBarConfiguration = AppBarConfiguration(navController.graph)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+
+            binding.fab.setOnClickListener { view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .setAnchorView(R.id.fab).show()
+            }
         }
 
         // View Image Activity (ThirdActivity)
@@ -67,6 +94,11 @@ class MainActivity : AppCompatActivity() {
                 displayToast("Permission denied. Cannot access Second Activity.")
             }
         }
+    }
+
+    private fun startSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 
     private fun displayToast(message: String) {
